c20e94994357f907a8a99690ce962e7307225ff0
kientnph41341 <kientnph41341@fpt.edu.vn>
2024-07-21 15:41:42 +0700

    Thông điệp commit của bạn

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DanhMucController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -16,19 +17,19 @@ use App\Http\Middleware\CheckRoleAdminMiddleware;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/danhmucs', function () {
+//     return view('admins.danhmucs.index');
+// });
 
 
 
 
-route::get('login',[AuthController::class,'showFromLogin']);
-route::post('login',[AuthController::class,'login'])->name('login');
+route::get('login', [AuthController::class, 'showFromLogin']);
+route::post('login', [AuthController::class, 'login'])->name('login');
 
-route::get('register',[AuthController::class,'showFromRegister']);
-route::post('register',[AuthController::class,'register'])->name('register');
-route::post('logout',[AuthController::class,'logout'])->name('logout');
+route::get('register', [AuthController::class, 'showFromRegister']);
+route::post('register', [AuthController::class, 'register'])->name('register');
+route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 
 // Route::get('/home', function () {
@@ -37,6 +38,36 @@ route::post('logout',[AuthController::class,'logout'])->name('logout');
 // route::get('/admin',function () {
 //     return 'Đây là trang admin';
 // })->middleware(['auth','auth.admin']);
+
+
+// Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// route Admin
+Route::middleware(['auth','auth.admin'])->prefix('admins')
+    ->as('admins.')
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admins.dashboard');
+        })->name('dashboard');
+
+        Route::prefix('danhmucs')
+            ->as('danhmucs.')
+            ->group(function () {
+                Route::get('/', [DanhMucController::class,'index'])->name('index');
+                Route::get('/create', [DanhMucController::class,'create'])->name('create');
+                Route::post('/store', [DanhMucController::class,'store'])->name('store');
+                Route::get('/show/{id}', [DanhMucController::class,'show'])->name('show');
+                Route::get('{id}/edit', [DanhMucController::class,'edit'])->name('edit');
+                Route::put('{id}/update', [DanhMucController::class,'update'])->name('update');
+                Route::delete('{id}/destroy', [DanhMucController::class,'destroy'])->name('destroy');
+
+            });
+    });
+
+
+
 
 // Route::middleware('auth')->group(function (){
 //     Route::get('/home', function () {
@@ -52,7 +83,3 @@ route::post('logout',[AuthController::class,'logout'])->name('logout');
 //         });
 //     });
 // });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
